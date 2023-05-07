69f8faeaa7bf87a078a3133032df65056f0a491c
Gerasimcuk <artemgerasimcuk11@gmail.com>
2023-05-07 11:16:30 +0300

    First Commit
    First Commit

---
+++ lab_1.ПР.5.4/лаб 1.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Батько: Герасимчук Юра Васильвич 1969 року народження,військовослубовець");
+        System.out.println("Мати: Герасимчук Жанна Євгенівна 1969 року народження підприємець");
+    }
+}
+
+
